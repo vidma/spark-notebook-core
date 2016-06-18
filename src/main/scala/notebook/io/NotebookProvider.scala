69f5d0039cd713ee02066fdd23bd0339826b711f
Gerard Maas <maasg@users.noreply.github.com>
2016-06-18 08:20:18 +0200

    (Cherry pick merge of listing filtering feature)
    Adds filtering capabilities to list (#7)
    
    * adds filtering capabilities to list
    
    * adds coverage for the filter funtion. All tests passing
    
    * makes listing work with Path operations
    
    * adds a case for literals
    
    * silly whitespaces
    
    * makes the interface more explicit, requiring regex instead of String
    
    * refactors impl to have a white list policy
    
    * adds default implementation

--- src/main/scala/notebook/io/NotebookProvider.scala
+++ src/main/scala/notebook/io/NotebookProvider.scala
@@ -18,6 +18,8 @@ trait Configurable[T] {
 
 trait NotebookProvider {
 
+  import NotebookProvider._
+
   def verifyProvider(): Future[Unit] = Future.successful(())
 
   def root: Path
@@ -30,34 +32,33 @@ trait NotebookProvider {
 
   def save(path: Path, notebook: Notebook)(implicit ev: ExecutionContext): Future[Notebook]
 
-  def list(path: Path)(implicit ev: ExecutionContext): Future[List[Resource]] = {
-    val lengthToRoot = root.toFile.getAbsolutePath.length
-    def dropRoot(f: java.io.File) = f.getAbsolutePath.drop(lengthToRoot).dropWhile(_ == '/')
-
-    Future {
-      val ps: List[java.io.File] = Option(root.resolve(path).toFile.listFiles)
-        .filter(_.length != 0) //toList fails if listFils is empty
-        .map(_.toList)
-        .getOrElse(Nil)
-      ps.map { f =>
-        val n = f.getName
-        if (f.isFile && n.endsWith(".snb")) {
-          NotebookResource(n.dropRight(".snb".length), dropRoot(f))
-        } else if (f.isFile) {
-          GenericFile(n, dropRoot(f), "file")
-        } else {
-          Repository(n, dropRoot(f))
-        }
-      }
-    }
+  private [io] lazy val listFilter = new FileFilter() {
+    override def accept(file:File): Boolean =  listingPolicy(file)
   }
 
-}
+  def list(path: Path) : Future[List[Resource]] = {
+    def relativePath(f: java.io.File): String = root.relativize(Paths.get(f.getAbsolutePath)).toString
+    Future {
 
+      Option(root.resolve(path).toFile.listFiles(listFilter))
+        .filter(_.length > 0) //toList fails if listFils is empty
+        .map(_.toList)
+        .getOrElse(Nil)
+        .map(f => (f.getName,relativePath(f),f))
+        .collect {
+          case (name, relPath, file) if isNotebookFile(file) =>
+            NotebookResource(name.dropRight(".snb".length), relPath)
+          case (name, relPath, file)  if (file.isFile) => GenericFile(name, relPath, "file")
+          case (name, relPath, file) => Repository(name, relPath)
+        }
+    }
+  }
+}
 
 object NotebookProvider {
   val isNotebookFile: File => Boolean = f => f.isFile && f.getName.endsWith(".snb")
   val isVisibleDirectory: File => Boolean = f => f.isDirectory && !f.getName.startsWith(".")
   val DefaultListingPolicy = (f:File) => isNotebookFile(f) || isVisibleDirectory(f)
 }
+
 
--- src/test/scala/notebook/io/NotebookProviderTests.scala
+++ src/test/scala/notebook/io/NotebookProviderTests.scala
@@ -9,11 +9,16 @@ import scala.concurrent.ExecutionContext
 import scala.concurrent.ExecutionContext.Implicits.global
 import org.apache.commons.io.FileUtils
 import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.prop.TableDrivenPropertyChecks
+import org.scalatest.prop.Tables.Table
 import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpec}
-import notebook.{GenericFile, Notebook, NotebookResource}
+import notebook.{GenericFile, Notebook, NotebookResource, Repository}
 import org.scalatest.time.{Millis, Seconds, Span}
 
-class NotebookProviderTests extends WordSpec with Matchers with BeforeAndAfterAll with ScalaFutures {
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+
+class NotebookProviderTests extends WordSpec with Matchers with BeforeAndAfterAll with ScalaFutures with TableDrivenPropertyChecks {
 
   implicit val defaultPatience =
     PatienceConfig(timeout =  Span(2, Seconds), interval = Span(5, Millis))
@@ -23,24 +28,30 @@ class NotebookProviderTests extends WordSpec with Matchers with BeforeAndAfterAl
   val emptyPath = rootPath.resolve("empty")
   val notebookFile = "nb.snb"
   val resourceFile = "res.res"
+  val dir = "dir"
+  val hiddenDir = ".hidden"
+  val hiddenGitDir = ".git"
+  val dirPath = notebookPath.resolve(dir)
+  val hiddenPath = notebookPath.resolve(hiddenDir)
+  val hiddenGitPath = notebookPath.resolve(hiddenGitDir)
 
-  override def beforeAll() : Unit = {
-    Seq(notebookPath, emptyPath).foreach(path => Files.createDirectory(path))
-    Seq(notebookFile, resourceFile).foreach{file =>
+  override def beforeAll(): Unit = {
+    Seq(notebookPath, emptyPath, dirPath, hiddenPath, hiddenGitPath).foreach(path => Files.createDirectory(path))
+    Seq(notebookFile, resourceFile).foreach { file =>
       val filePath = notebookPath.resolve(file)
       Files.write(filePath, "text".getBytes(StandardCharsets.UTF_8))
     }
   }
 
-  override def afterAll() : Unit = {
+  override def afterAll(): Unit = {
     FileUtils.deleteDirectory(rootPath.toFile)
   }
 
   class BaseProvider extends NotebookProvider {
     override def root: Path = rootPath
-    override def get(path: Path) (implicit ev:ExecutionContext): Future[Notebook] = ???
-    override def delete(path: Path) (implicit ev:ExecutionContext): Future[Notebook] = ???
-    override def save(path: Path, notebook: Notebook) (implicit ev:ExecutionContext): Future[Notebook] = ???
+    override def get(path: Path)(implicit ev: ExecutionContext): Future[Notebook] = ???
+    override def delete(path: Path)(implicit ev: ExecutionContext): Future[Notebook] = ???
+    override def save(path: Path, notebook: Notebook)(implicit ev: ExecutionContext): Future[Notebook] = ???
   }
 
   val T = true
@@ -67,25 +78,87 @@ class NotebookProviderTests extends WordSpec with Matchers with BeforeAndAfterAl
 
   "Notebook provider" should {
 
-    val defaultInstance = new BaseProvider()
+    "retrieve a list of resources from an empty path" in {
+      whenReady(providerDefault.list(emptyPath)) { files =>
+        files should be('empty)
+      }
+    }
+
 
     "retrieve a list of resources from a path" in {
-      whenReady(defaultInstance.list(notebookPath) ) { files =>
-        files.size should be (2)
-        files.foreach{
-          case nb: NotebookResource => nb.name should be (notebookFile.dropRight(".snb".size))
-          case res: GenericFile => res.name should be (resourceFile)
+      whenReady(providerNoFilter.list(notebookPath)) { resources =>
+        resources.size should be(5)
+        resources.foreach {
+          case nb: NotebookResource => nb.name should be(notebookFile.dropRight(".snb".size))
+          case res: GenericFile => res.name should be(resourceFile)
+          case rep: Repository => rep.name should (be(hiddenDir) or be(dir) or be(hiddenGitDir))
           case x => fail("unexpected resource" + x)
         }
       }
     }
 
-    "retrieve a list of resources from an empty path" in {
-      whenReady(defaultInstance.list(emptyPath) ) { files =>
-        files should be ('empty)
+    "retrieves a list of resources using default filtering" in {
+      whenReady(providerDefault.list(notebookPath)) { resources =>
+        resources.size should be (2)
+        resources.foreach {
+          case nb: NotebookResource => nb.name should be(notebookFile.dropRight(".snb".size))
+          case rep: Repository => rep.name should be(dir)
+          case x => fail("unexpected resource" + x)
+        }
+      }
+
+    }
+  }
+
+  "Notebook provider" should {
+
+    val defaultInstance = new BaseProvider()
+
+    "retrieves a list of resources filtered with a literal" in {
+      whenReady(providerExplicitFilter.list(notebookPath)) { resources =>
+        println(" Retrieved files: " + resources.mkString(", "))
+        resources.size should be (2)
+        resources.foreach {
+          case nb: NotebookResource => nb.name should be(notebookFile.dropRight(".snb".size))
+          case res: GenericFile => res.name should be(resourceFile)
+          case x => fail("unexpected resource" + x)
+        }
       }
     }
 
+    "retrieves a list of resources filtered with a pattern" in {
+      whenReady(providerPatternFilter.list(notebookPath)) { resources =>
+        resources.size should be (2)
+        resources.foreach {
+          case nb: NotebookResource => nb.name should be(notebookFile.dropRight(".snb".size))
+          case rep: Repository => rep.name should be(dir)
+          case x => fail("unexpected resource" + x)
+        }
+      }
+    }
+
+  }
+
+  val filterTable = Table(
+    ("filename", "no_filter", "explicit_filter", "pattern_filter"),
+    (".git",     T, F, F),
+    ("res.res",  T, T, F),
+    ("nt.snb",   T, T, T),
+    (".file",    T, F, F)
+  )
+  val withProvider: BaseProvider => String => Boolean = provider => filename =>
+    provider.listFilter.accept(new java.io.File(s"./$filename"))
+
+  "allow filenames following the filter criteria" in {
+    val explicitFiltering = withProvider(providerExplicitFilter)
+    val noFiltering = withProvider(providerNoFilter)
+    val patternFiltering = withProvider(providerPatternFilter)
+
+    forAll(filterTable) { (filename, noFilter, explicitFilter, patternFilter) =>
+      explicitFiltering(filename) should be(explicitFilter)
+      noFiltering(filename) should be(noFilter)
+      patternFiltering(filename) should be(patternFilter)
+    }
   }
 
 }
