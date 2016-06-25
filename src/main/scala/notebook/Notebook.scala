b1f7795b68dc4cb1bb875c2167dfa10444faa9ae
Gerard Maas <maasg@users.noreply.github.com>
2016-06-25 12:03:47 +0200

    failure explicit interface + increase Notebook abstraction (#8)
    * creates an abstraction for Notebook that's self-contained
    
    * adds coverage for empty notebook
    
    * adds coverage for empty notebook
    
    * adds coverage to errors
    
    * fine-tume usage of futures

---
+++ src/main/scala/notebook/Notebook.scala
@@ -0,0 +1,55 @@
+package notebook
+
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext
+import notebook.NBSerializer._
+
+trait Notebook {
+  def metadata: Option[Metadata] = None
+  def cells: Option[List[Cell]] = Some(Nil)
+  def worksheets: Option[List[Worksheet]] = None
+  def autosaved: Option[List[Worksheet]] = None
+  def nbformat: Option[Int]
+  def name: String = metadata.map(_.name).getOrElse("Anonymous")
+  def rawContent: Option[String] = None
+  def isFromRaw = rawContent.isDefined
+}
+
+
+object Notebook {
+
+  private[notebook] case class NotebookImpl( override val metadata: Option[Metadata] = None,
+                           override val cells: Option[List[Cell]] = Some(Nil),
+                           override val worksheets: Option[List[Worksheet]] = None,
+                           override val autosaved: Option[List[Worksheet]] = None,
+                           override val nbformat: Option[Int],
+                           override val rawContent: Option[String]
+                         ) extends Notebook
+
+  def apply(metadata: Option[Metadata] = None,
+            cells: Option[List[Cell]] = Some(Nil),
+            worksheets: Option[List[Worksheet]] = None,
+            autosaved: Option[List[Worksheet]] = None,
+            nbformat: Option[Int],
+            rawContent: Option[String]
+  ): Notebook = {
+      NotebookImpl(metadata, cells, worksheets, autosaved, nbformat, rawContent)
+  }
+
+  def read(str: String)(implicit ex : ExecutionContext): Future[Notebook] = {
+    NBSerializer.fromJson(str).map(nb =>  NotebookImpl(nb.metadata, nb.cells, nb.worksheets, nb.autosaved, nb.nbformat, Some(str)))
+  }
+
+  def write(nb: Notebook)(implicit ex : ExecutionContext): Future[String] = {
+    Future(nb.rawContent).flatMap {
+      case Some(content) => Future.successful(content)
+      case None =>
+        val snb = SerNotebook(nb.metadata, nb.cells, nb.worksheets, nb.autosaved, nb.nbformat)
+        NBSerializer.toJson(snb)
+    }
+  }
+}
+
+class NotebookDeserializationException(msg: String, cause: Throwable) extends Exception(msg, cause)
+class EmptyNotebookException extends Exception ()
+class NotebookNotFoundException(location:String) extends Exception(s"Notebook not found at $location")
--- src/test/scala/notebook/io/NotebookProviderTests.scala
+++ src/test/scala/notebook/io/NotebookProviderTests.scala
@@ -2,19 +2,21 @@ package notebook.io
 
 import java.nio.charset.StandardCharsets
 import java.nio.file.{Files, Path}
+import java.io.File
 
-import com.typesafe.config.ConfigFactory
-import notebook.NBSerializer.{GenericFile, Notebook, NotebookResource}
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext
+import scala.concurrent.ExecutionContext.Implicits.global
 import org.apache.commons.io.FileUtils
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpec}
-
-import scala.concurrent.Future
-
+import notebook.{GenericFile, Notebook, NotebookResource}
+import org.scalatest.time.{Millis, Seconds, Span}
 
 class NotebookProviderTests extends WordSpec with Matchers with BeforeAndAfterAll with ScalaFutures {
 
-  import scala.concurrent.ExecutionContext.Implicits.global
+  implicit val defaultPatience =
+    PatienceConfig(timeout =  Span(2, Seconds), interval = Span(5, Millis))
 
   val rootPath = Files.createTempDirectory("notebook-provider-test")
   val notebookPath = rootPath.resolve("notebook")
@@ -34,15 +36,38 @@ class NotebookProviderTests extends WordSpec with Matchers with BeforeAndAfterAl
     FileUtils.deleteDirectory(rootPath.toFile)
   }
 
+  class BaseProvider extends NotebookProvider {
+    override def root: Path = rootPath
+    override def get(path: Path) (implicit ev:ExecutionContext): Future[Notebook] = ???
+    override def delete(path: Path) (implicit ev:ExecutionContext): Future[Notebook] = ???
+    override def save(path: Path, notebook: Notebook) (implicit ev:ExecutionContext): Future[Notebook] = ???
+  }
+
+  val T = true
+  val F = false
+
+  val providerDefault = new BaseProvider()
+
+  val providerNoFilter = new BaseProvider(){
+    override val listingPolicy = (f:File) => true
+  }
+
+  val providerExplicitFilter = new BaseProvider() {
+    override val listingPolicy = (f:File) => (f.getName endsWith ".snb") || (f.getName startsWith "res")
+  }
+
+  val providerPatternFilter = new BaseProvider() {
+    override val listingPolicy = (f:File) =>
+      if (f.isDirectory) {
+        !f.getName.startsWith(".")
+      } else {
+        f.getName.endsWith(".snb")
+    }
+  }
+
   "Notebook provider" should {
 
-    val defaultInstance = new NotebookProvider() {
-      override def root: Path = rootPath
-      val successfulNothing : Future[Option[Notebook]] = Future.successful(None)
-      override def get(path: Path)(implicit ev: scala.concurrent.ExecutionContext): Future[Option[Notebook]] = successfulNothing
-      override def delete(path: Path)(implicit ev: scala.concurrent.ExecutionContext): Future[Option[Notebook]] = successfulNothing
-      override def save(path: Path, notebook: Notebook)(implicit ev: scala.concurrent.ExecutionContext): Future[Option[Notebook]] = successfulNothing
-    }
+    val defaultInstance = new BaseProvider()
 
     "retrieve a list of resources from a path" in {
       whenReady(defaultInstance.list(notebookPath) ) { files =>
